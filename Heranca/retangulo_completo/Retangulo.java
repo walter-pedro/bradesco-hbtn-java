f5346df7c20a0d0f76e7b39da186210a0598243d
Walter Pedro <wal100lp@hotmail.com>
2025-07-10 16:23:25 +0000

    Implementing exercise 4

---
+++ Heranca/retangulo_completo/Retangulo.java
@@ -0,0 +1,36 @@
+public class Retangulo extends FormaGeometrica {
+
+    private double largura;
+    private double altura;
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        if (largura < 0 ) {
+            throw new java.lang.IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if (altura < 0 ) {
+            throw new java.lang.IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        }
+        this.altura = altura;
+    }
+
+    @Override
+    public double area() {
+        return this.largura * this.altura;
+    }
+
+    public String toString() {
+        return String.format("[Retangulo] %f / %f", this.largura, this.altura);
+    }
+}
