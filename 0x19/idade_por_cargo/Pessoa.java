6d76defe98ab54c7c5f8674129349f45e5c2605c
Walter Pedro <wal100lp@hotmail.com>
2025-08-26 16:45:08 +0000

    Implementing 'Java - Collectors' Exercises

---
+++ 0x19/idade_por_cargo/Pessoa.java
@@ -0,0 +1,63 @@
+import java.util.Locale;
+
+public class Pessoa {
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(new Locale("pt", "BR"),"[%d] %s %s %d R$ %.2f", codigo, nome, cargo, idade, salario);
+    }
+}
