766cbb6e90b1a0f37d6e9b1698cb031364c05cf4
Walter Pedro <wal100lp@hotmail.com>
2025-08-26 16:47:21 +0000

    Fix 'Java - Collectors' Exercises

--- 0x19/idade_por_cargo/Pessoa.java
+++ 0x19/idade_por_cargo/Pessoa.java
@@ -58,6 +58,6 @@ public class Pessoa {
 
     @Override
     public String toString() {
-        return String.format(new Locale("pt", "BR"),"[%d] %s %s %d R$ %.2f", codigo, nome, cargo, idade, salario);
+        return String.format(new Locale("pt", "BR"),"[%d] %s %s %d R$ %f", codigo, nome, cargo, idade, salario);
     }
 }
