387d8cc4b368f2705ed01569eeb0cb7eb688b769
Walter Pedro <wal100lp@hotmail.com>
2025-09-08 13:54:14 +0000

    Fixing 'Java Multithreading' exercises

--- threads/0/Principal.java
+++ threads/0/Principal.java
@@ -8,6 +8,13 @@ public class Principal {
         thread1.start();
         thread2.start();
 
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         System.out.printf("Valor final do contador: %d", contador.getContagem());
     }
 }
