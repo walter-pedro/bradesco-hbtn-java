62ff6231cf8a99194d1d568f97352bdebcc8473b
Walter Pedro <wal100lp@hotmail.com>
2025-10-01 13:05:06 +0000

    Implementing service methods

--- ddd/0/application/PedidoService.java
+++ ddd/0/application/PedidoService.java
@@ -18,21 +18,29 @@ public class PedidoService {
 
 
     public Pedido criarPedido(Cliente cliente) {
-        // implementar
+        Pedido pedido = new Pedido(cliente);
+        pedidoRepository.salvar(pedido);
+        return pedido;
     }
 
 
     public void adicionarProduto(String pedidoId, Produto produto) {
-        // implementar
+        Pedido pedido = pedidoRepository.buscarPorId(pedidoId);
+        pedido.adicionarProduto(produto);
+        pedidoRepository.salvar(pedido);
     }
 
 
     public void pagarPedido(String pedidoId) {
-        // implementar
+        Pedido pedido = pedidoRepository.buscarPorId(pedidoId);
+        pedido.pagar();
+        pedidoRepository.salvar(pedido);
     }
 
 
     public void cancelarPedido(String pedidoId) {
-        // implementar
+        Pedido pedido = pedidoRepository.buscarPorId(pedidoId);
+        pedido.cancelar();
+        pedidoRepository.salvar(pedido);
     }
 }
